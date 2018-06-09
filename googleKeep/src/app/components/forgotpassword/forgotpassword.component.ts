d0d83ddc6c38f060aaa6634fd9eb7515c51abf37
SanaShaikh28 <SanaShaikh28>
2018-06-09 15:47:12 +0530

    Added forgot password component

--- googleKeep/src/app/components/forgotpassword/forgotpassword.component.ts
+++ googleKeep/src/app/components/forgotpassword/forgotpassword.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { ViewEncapsulation } from '@angular/core';
+import { ForgotPassService } from '../../services/forgotpass.service';
 
 @Component({
   selector: 'app-forgotpassword',
   templateUrl: './forgotpassword.component.html',
-  styleUrls: ['./forgotpassword.component.css']
+  styleUrls: ['./forgotpassword.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class ForgotpasswordComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'Email ID/Username cannot be left blank' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+  constructor(private forgotServiceObj: ForgotPassService) { }
 
   ngOnInit() {
   }
 
+
+/**@method:This method is to call forgot password Api*/
+forgot():void
+{
+  console.log(this.model);
+  this.forgotServiceObj.forgot(this.model)
+                        .subscribe(response=>{
+                          console.log("Link sent successfully");
+        });
+}
 }
